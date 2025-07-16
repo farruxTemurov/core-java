a881ad759b3b6a0d982eea2ad18159f4125cc732
Farrux Temurov <tfarrux70@gmail.com>
2025-07-16 12:51:08 +0500

    session 20 about abstraction and inheritance

---
+++ Session20/src/com/PaymentsApp.java
@@ -0,0 +1,51 @@
+package com;
+// abstraction and injeritence
+// EStore extends payTm and gets the messages from it
+
+abstract class PayTmPaymentGateway {
+	PayTmPaymentGateway() {
+		System.out.println("[PayTM] object constructed");
+	}
+
+	private boolean isBankInterfaceUp = false;
+
+	void pay(int amount) {
+		if (isBankInterfaceUp) {
+			System.out.println("[PayTM] payment successfull with amount of " + amount);
+			onSuccess("101 - Payment success");
+		} else {
+			System.out.println("[PayTM] payment failed.");
+			onFailure("201 -  payment failure.");
+		}
+	}
+
+	abstract void onSuccess(String message);
+
+	abstract void onFailure(String message);
+}
+
+class EStorePayment extends PayTmPaymentGateway {
+	EStorePayment() {
+		System.out.println("[EStore] object constructed");
+	}
+
+	void onSuccess(String message) {
+		System.out.println(message);
+		System.out.println("[EStore] Thank you for placing an order");
+	}
+
+	void onFailure(String message) {
+		System.out.println("[EStore] " + message);
+		System.out.println("[EStore] Something went wrong");
+	}
+}
+
+public class PaymentsApp {
+
+	public static void main(String[] args) {
+		EStorePayment estorePayment = new EStorePayment();
+		System.out.println("");
+		estorePayment.pay(3000);
+	}
+
+}
