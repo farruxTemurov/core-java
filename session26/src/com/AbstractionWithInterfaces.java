de884be9ecbd9ac26fefaca9d22e0053d28a4397
Farrux Temurov <tfarrux70@gmail.com>
2025-08-09 17:32:25 +0500

    classes converted to interfaces

--- session26/src/com/AbstractionWithInterfaces.java
+++ session26/src/com/AbstractionWithInterfaces.java
@@ -1,19 +1,22 @@
 package com;
 
-abstract class GooglePay {
-	void payFromGoogle(int amount) {
+//abstract class GooglePay {
+interface GooglePay {
+	default void payFromGoogle(int amount) {
 		System.out.println("[GooglePay] amount paid " + amount);
 		onSuccess(101, "Transaction successful");
 	}
 
-	abstract void onSuccess(int code, String message);
+	abstract void onSuccess(int code, String message); // -> by default public abstract
 
 	abstract void onFailure(int code, String message);
 }
 
-abstract class ApplePay {
-	void payFromApple(int amount) {
+//abstract class ApplePay {
+interface ApplePay {
+	default void payFromApple(int amount) {
 		System.out.println("[ApplePay] amount paid " + amount);
+		onFailure("Bank interface down. error code of 3102");
 	}
 
 	abstract void onSuccess(String message);
@@ -21,22 +24,38 @@ abstract class ApplePay {
 	abstract void onFailure(String message);
 }
 
-class FoodDeliveryAppPayments extends GooglePay {
+class FoodDeliveryAppPayments implements GooglePay, ApplePay {
 	@Override
-	void onSuccess(int code, String message) {
+	public void onSuccess(int code, String message) {
 		System.out.println("[FDA] Payment received. We'll deliver the order soon... " + code + " " + message);
 	}
 
 	@Override
-	void onFailure(int code, String message) {
+	public void onFailure(int code, String message) {
 		System.out.println("[FDA] Payment failed. Please try again... " + code + " " + message);
+	}
+
+	@Override
+	public void onSuccess(String message) {
+		System.out.println("[FDA] Payment received");
+
+	}
+
+	@Override
+	public void onFailure(String message) {
+		System.out.println("[FDA] Payment failed -" +message);
+
 	}
 }
 
 public class AbstractionWithInterfaces {
 
 	public static void main(String[] args) {
-
+		/*
+		 * GooglePay ref = new FoodDeliveryAppPayments(); ref.payFromGoogle(3000);
+		 */
+		ApplePay ref = new FoodDeliveryAppPayments();
+		ref.payFromApple(4200);
 	}
 
 }
