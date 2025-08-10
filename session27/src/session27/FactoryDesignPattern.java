a1110673c71f952bc14b9fb90e676e7df2207fb1
Farrux Temurov <tfarrux70@gmail.com>
2025-08-10 15:33:21 +0500

    implemented plan factory to test 3G and 4G plans

--- session27/src/session27/FactoryDesignPattern.java
+++ session27/src/session27/FactoryDesignPattern.java
@@ -9,8 +9,8 @@ interface Plan {
 
 	default void purchase(int amount) {
 		if (amount == getPlanPrice()) {
-			System.out.println("Purchasing the plan " + getPlanName());
-			System.out.println("Plan purchased successfully for an amount " + amount);
+			System.out.println("Purchasing the plan: " + getPlanName());
+			System.out.println("Plan purchased successfully for an amount: " + amount);
 			onPurchse("Plan activated from " + new Date());
 		} else {
 			System.out.println("Purchase Failed. Amount mismatched");
@@ -58,10 +58,28 @@ class Plan4G implements Plan {
 
 }
 
+class PlanFactory {
+	public static Plan getPlan(int typeOfPlan) {
+		Plan plan = null;
+
+		if (typeOfPlan == 3) {
+			plan = new Plan3G();
+		} else if (typeOfPlan == 4) {
+			plan = new Plan4G();
+		} else {
+			System.out.println("Invalid Plan");
+		}
+		return plan;
+	}
+}
+
 public class FactoryDesignPattern {
 
 	public static void main(String[] args) {
-
+		Plan plan = PlanFactory.getPlan(3);
+		if (plan != null) {
+			plan.purchase(250);
+		}
 	}
 
 }
