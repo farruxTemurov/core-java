a06bec83636479554f04e2a2401d8fbc3d082351
Farrux Temurov <tfarrux70@gmail.com>
2025-10-23 21:59:10 +0200

    a small tweak

--- extraPractice/src/com/countOccurrences.java
+++ extraPractice/src/com/countOccurrences.java
@@ -4,16 +4,16 @@ public class countOccurrences {
 
 	public static void main(String[] args) {
 		String[] words = { "apple", "banana", "apple", "orange" };
-		int count = countOccurrences(words, "orange");
-		System.out.println("Count of orange is " + count);
+		int count = countOccurrences(words, "apple");
+		System.out.println("Count of apple is " + count);
 	}
 
 	public static <T> int countOccurrences(T[] array, T element) {
 		int count = 0;
-		for(T item : array) {
-			if(item == null && element == null) {
+		for (T item : array) {
+			if (item == null && element == null) {
 				count++;
-			} else if(item != null && item.equals(element)) {
+			} else if (item != null && item.equals(element)) {
 				count++;
 			}
 		}
